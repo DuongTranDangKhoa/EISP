c623996e58fceb0a75ccbf75691f57dfda4751c4
lengocson <leson131002@gmail.com>
2023-07-13 15:52:25 +0700

    Fix cors

--- src/main/java/com/swp391/demo/filter/CorsFilter.java
+++ src/main/java/com/swp391/demo/filter/CorsFilter.java
@@ -13,28 +13,14 @@ public class CorsFilter implements Filter {
         // No initialization required
     }
 
-    // @Override
-    // public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-    //     HttpServletRequest request = (HttpServletRequest) servletRequest;
-    //     HttpServletResponse response = (HttpServletResponse) servletResponse;
-    //     response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    //     response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    //     response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    //     if (request.getMethod().equals("OPTIONS")) {
-    //         response.setStatus(HttpServletResponse.SC_ACCEPTED);
-    //         return;
-    //     }
-    //     filterChain.doFilter(servletRequest, servletResponse);
-    // }
-
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        response.setHeader("Access-Control-Allow-Origin", "https://localhost:5001, https://localhost:3000");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
