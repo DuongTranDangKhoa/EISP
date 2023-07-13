6260e4bcc902f457174735aead9343fac248cd8c
Nikin-2204 <truonglnh.kv@gmail.com>
2023-07-13 15:39:02 +0700

    Update CorsFilter.java

--- src/main/java/com/swp391/demo/filter/CorsFilter.java
+++ src/main/java/com/swp391/demo/filter/CorsFilter.java
@@ -13,18 +13,36 @@ public class CorsFilter implements Filter {
         // No initialization required
     }
 
+    // @Override
+    // public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    //     HttpServletRequest request = (HttpServletRequest) servletRequest;
+    //     HttpServletResponse response = (HttpServletResponse) servletResponse;
+    //     response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    //     response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    //     response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    //     if (request.getMethod().equals("OPTIONS")) {
+    //         response.setStatus(HttpServletResponse.SC_ACCEPTED);
+    //         return;
+    //     }
+    //     filterChain.doFilter(servletRequest, servletResponse);
+    // }
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+        response.setHeader("Access-Control-Allow-Origin", "https://localhost:5001, https://localhost:3000");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-        if (request.getMethod().equals("OPTIONS")) {
-            response.setStatus(HttpServletResponse.SC_ACCEPTED);
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
             return;
+        } else {
+            filterChain.doFilter(request, response);
         }
-        filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
