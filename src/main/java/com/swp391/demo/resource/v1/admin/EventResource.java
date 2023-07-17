a72fe86538d61f7bfea170a6827c862699862258
Nikin-2204 <truonglnh.kv@gmail.com>
2023-07-18 01:10:25 +0700

    check Date valid

--- src/main/java/com/swp391/demo/resource/v1/admin/EventResource.java
+++ src/main/java/com/swp391/demo/resource/v1/admin/EventResource.java
@@ -5,6 +5,7 @@
 package com.swp391.demo.resource.v1.admin;
 
 import com.swp391.demo.dao.EventDAO;
+import com.swp391.demo.dto.ErrorDTO;
 import com.swp391.demo.dto.EventDTO;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
@@ -13,6 +14,7 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import java.sql.Date;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -24,12 +26,23 @@ import java.util.List;
 public class EventResource {
 
     private EventDAO dao = EventDAO.getInstance();
+    private Date date;
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response createEvent(EventDTO dto) throws SQLException {
-        if (dto.getBeginDate().after(dto.getEndDate())) {
-            return Response.ok(new Error("Date not vaild")).build();
+        long millis = System.currentTimeMillis();
+        date = new java.sql.Date(millis);
+        System.out.println(date);
+        
+        
+      
+        if (dto.getBeginDate().before(date)) {
+            return Response.ok(new ErrorDTO("Date not vaild")).build();
+        }
+        if (dto.getEndDate().before(dto.getBeginDate())) {
+            return Response.ok(new ErrorDTO("Date not vaild")).build();
         }
         boolean result = dao.createEvent(dto);
         if (result) {
@@ -54,9 +67,16 @@ public class EventResource {
     @Path("update")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response updateEvent(EventDTO dto) throws SQLException {
-        if (dto.getBeginDate().after(dto.getEndDate())) {
-            return Response.ok(new Error("Date not vaild")).build();
+        long millis = System.currentTimeMillis();
+        date = new java.sql.Date(millis);
+
+        if (dto.getBeginDate().before(date)) {
+            return Response.ok(new ErrorDTO("Date not vaild")).build();
+        }
+        if (dto.getEndDate().before(dto.getBeginDate())) {
+            return Response.ok(new ErrorDTO("Date not vaild")).build();
         }
         boolean result = dao.updateEvent(dto);
         if (result) {
